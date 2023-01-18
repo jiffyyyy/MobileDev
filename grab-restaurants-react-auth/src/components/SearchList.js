0bb16363ad9c3c14ca6ee489988c2304cd50ccb3
jiffyyyy <101688755+jiffyyyy@users.noreply.github.com>
2023-01-18 17:25:50 +0700

    Add files via upload

---
+++ grab-restaurants-react-auth/src/components/SearchList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "./Card";
+
+const SearchList = ({ filteredRestaurants, handleDelete }) => {
+  const filtered = filteredRestaurants.map((restaurant) => {
+    return (
+      <Card
+        key={restaurant.id}
+        restaurant={restaurant}
+        handleDelete={handleDelete}
+      />
+    );
+  });
+  return <>{filtered}</>;
+};
+
+export default SearchList;
