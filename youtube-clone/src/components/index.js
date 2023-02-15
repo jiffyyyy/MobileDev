df3ef14ea7de864b4ac850cad9048cd48e7ea142
jiffyyyy <101688755+jiffyyyy@users.noreply.github.com>
2023-02-15 17:00:12 +0700

    Delete youtube-clone directory

--- youtube-clone/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as NavBar} from "./NavBar"
-export { default as SearchBar } from "./SearchBar";
-export { default as SideBar } from "./SideBar";
-export { default as Videos} from "./Videos"
-export { default as VideoCard} from "./VideoCard"
-export { default as Loader} from "./Loader"
