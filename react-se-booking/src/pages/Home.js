78c3484d00adcdcedbb6237ae6b000e692bf14ec
jiffyyyy <101688755+jiffyyyy@users.noreply.github.com>
2023-03-08 16:46:44 +0700

    Add files via upload

---
+++ react-se-booking/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import Header from '../components/Header'
+const Home = () => {
+  return (
+    <div>
+        <NavBar/>
+        <Header/>
+    </div>
+  )
+}
+
+export default Home
